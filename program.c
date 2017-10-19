cba176667afe8450878f38b8bc2268f7353e54b5
marwawer <marwawer@hotmail.com>
2017-10-19 16:04:48 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,14 @@
+/*dolacz biblioteke standardowa input-output*/
+#include <stdio.h>
+
+/*y=f(x)*/
+/* main () => 0*/
+
+int main ()  /*funkcja główna programu, zwraca wartość calkowita*/
+{ /* miedzy klamrami jest cialo funkcji*/
+
+	puts("Pierwszy program");	/*funkcja z biblioteki stdio*/
+
+	return 0; /*main zwraca wartosc 0*/
+
+}
